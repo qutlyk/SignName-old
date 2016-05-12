6e7b4a6ff7f724a5694811b236d0e72eafaa745f
skyADMIN <546485299@qq.com>
2016-05-12 13:21:51 +0800

    add create method

--- src/main/java/com/lihuanyu/signin/controller/SignController.java
+++ src/main/java/com/lihuanyu/signin/controller/SignController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by skyADMIN on 16/3/19.
@@ -19,23 +20,33 @@ public class SignController {
     @Autowired
     private SignService signService;
 
+    @RequestMapping("/create")
+    public String showCreatePage() {
+        return "create";
+    }
+
+    @RequestMapping(value = "/docreate",method = RequestMethod.POST)
+    public String doCreate(String actname){
+        return signService.doCreate(actname);
+    }
+
     @RequestMapping("/sign")
-    public String signProcess(String id){
-        if (loginService.isLogin()){
+    public String signProcess(String id) {
+        if (loginService.isLogin()) {
             //处理签到过程
             return "error";
-        }else {
+        } else {
             return "error";
         }
     }
 
     @RequestMapping("/success")
-    public String success(Model model){
-        return signService.signResult(model,"签到成功","感谢您按时参会.");
+    public String success(Model model) {
+        return signService.signResult(model, "签到成功", "感谢您按时参会.");
     }
 
     @RequestMapping("/false")
-    public String fail(Model model){
-        return signService.signResult(model,"签到失败或已经签到","请勿重复签到.");
+    public String fail(Model model) {
+        return signService.signResult(model, "签到失败或已经签到", "请勿重复签到.");
     }
 }
--- src/main/java/com/lihuanyu/signin/service/SignService.java
+++ src/main/java/com/lihuanyu/signin/service/SignService.java
@@ -1,10 +1,13 @@
 package com.lihuanyu.signin.service;
 
+import com.lihuanyu.signin.model.CreateList;
+import com.lihuanyu.signin.model.CreateListDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
 import javax.servlet.http.HttpSession;
+import java.sql.Timestamp;
 
 /**
  * Created by skyADMIN on 16/5/7.
@@ -15,13 +18,31 @@ public class SignService {
     @Autowired
     private HttpSession httpSession;
 
-    public String signResult(Model model,String result, String word){
+    @Autowired
+    private CreateListDao createListDao;
+
+    public String doCreate(String actName) {
+        CreateList createList = new CreateList();
+        createList.setActivityname(actName);
+        int yibanid = (int) httpSession.getAttribute("userid");
+        createList.setYibanid(yibanid);
+        createList.setSigned_time(new Timestamp(System.currentTimeMillis()));
+        try {
+            createListDao.save(createList);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return "error";
+        }
+        return "index";
+    }
+
+    public String signResult(Model model, String result, String word) {
         String headurl = (String) httpSession.getAttribute("yibanhead");
         String username = (String) httpSession.getAttribute("username");
-        model.addAttribute("result",result);
-        model.addAttribute("username",username);
-        model.addAttribute("photo",headurl);
-        model.addAttribute("word",word);
+        model.addAttribute("result", result);
+        model.addAttribute("username", username);
+        model.addAttribute("photo", headurl);
+        model.addAttribute("word", word);
         return "index";
     }
 }
